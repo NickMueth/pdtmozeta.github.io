53372e12a5140e1e191c68add6d754bb20f0167b
NickMueth <musicman2797@gmail.com>
2017-11-28 14:45:02 -0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>PDT MO Zeta</title>
+	</head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<body>
+		<style>
+		body{
+			background-color: #d2f5ff;
+		}
+		
+		/*navigation bar*/
+		.topnav{
+			overflow: hidden;
+			background-color:#BCC6CC;
+		}
+		/*navigation bar links*/
+		.topnav a{
+			float: left;
+			display: block;
+			color: #f2f2f2;
+			text-align:center;
+			padding: 14px 16px;
+			text-decoration: none;
+		}
+		.menuitem{
+			background-color:#BCC6CC;
+			padding:8px;
+			margin-top:7px;
+		}
+		.main{
+			float:left;
+			width:60%;
+			padding:0 20px;
+		}
+		.right{
+			background-color:#BCC6CC;
+			float:right;
+			width:25%;
+			padding:8px;
+			margin-top:7px;
+			text-align:center;
+		}
+		
+		/*Change colors for links when hovering*/
+		.topnav a:hover{
+			background-color: #ddd;
+			color: black;
+		}
+		
+		/*Color for active link*/
+		.topnav a.active{
+			background-color: #4CAF50;
+			color: white;
+		}
+		
+		@media only screen and (max-width:620px){
+			/* For mobile phones: */
+			.menu,.main,.right{
+				width:100%;
+			}
+		}
+		img{
+			width:100%;
+		}
+		h1{
+			color: white;
+			text-align: center;
+			font-size: 60px;
+		}
+		h3{
+			color: red;
+			text-align: center;
+			font-size: 60px;
+		p{
+			font-family: verdana;
+			font-size: 20px;
+			}
+		</style>
+		<div style="background-color:#47b8ff; padding: 5px; text-align:center;">
+			<h1>&Phi; &Delta; &Theta;</h1>
+			<!--<img src="header.jpg" width="944" height="184">-->
+			<!--<h1>Phi Delta Theta - MO Zeta Chapter - Southeast Missouri State University</h1>-->
+		</div>
+		
+		<div style="overflow:auto">
+		
+		<!--Menu-->
+		<div class="topnav">
+			<a href="#">Gallery</a>
+			<a href="#">Our Exec Board</a>
+			<a href="#">Social Media</a>
+			<a href="#">About</a>
+			<a href="history.html">History</a>
+			<a href="#">Contact Us</a>
+		</div>
+		
+		
+		
+		<!--Right side-->
+		<div class="right">
+			<h2>Twitter Feed</h2>
+			<p>There will eventually be a twitter feed from our official twitter here</p>
+		</div>
+		</div>
+		<!--<p>AJ McCracken is a doofus</p>
+		<a href="http://pdtmozeta.weebly.com/">PDT MO Zeta Weebly</a>
+		<h3>Impeach Elwood</h3>-->
+		
+	</body>
+	<!--
+	Just things to know:
+		Color Codes:
+			Azure: #007FFF
+			Argent: #868992
+		Greek Letters:
+			&Phi;
+			&Delta;
+			&Theta;
+	-->
+</html>
